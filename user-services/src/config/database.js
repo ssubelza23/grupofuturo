6f13cb7623cae7aeb8ca44afc8d2ed854b9cb521
ssubelza23 <ssubelza23@gmail.com>
2025-02-09 10:15:20 -0300

    script bd users

--- user-services/src/config/database.js
+++ user-services/src/config/database.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.USER_DB_USER,
-  host: process.env.USER_DB_HOST, // Utiliza la variable de entorno POSTGRES_HOST
-  database: process.env.USER_DB_NAME,
-  password: process.env.USER_DB_PASSWORD, 
+  user: process.env.POSTGRES_USER, 
+  host: process.env.POSTGRES_HOST || 'db_user', 
+  database: process.env.POSTGRES_DB, 
+  password: process.env.POSTGRES_PASSWORD, 
   port: 5432,
 });
 
