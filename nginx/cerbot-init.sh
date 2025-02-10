e9fbf678fbf5892f11a2df235ef4e0a3558974a0
ssubelza23 <ssubelza23@gmail.com>
2025-02-10 19:44:37 -0300

    nginx

--- nginx/cerbot-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# filepath: /c:/Recuperado/grupoFuturo/nginx/certbot-init.sh
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot --webroot-path=/var/www/certbot \
-  --email ssubelza23@gmail.com --agree-tos --no-eff-email \
-  -d www.grupofuturo.com.ar -d grupofuturo.com.ar" certbot
