46a81d1676f064f8f59b0d13771b6723c116754d
ssubelza23 <ssubelza23@gmail.com>
2025-02-13 13:37:37 -0300

    final

--- map-services/src/app.js
+++ map-services/src/app.js
@@ -12,17 +12,13 @@ const uploadRoutes = require('./routes/uploadRoutes');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Configurar CORS para aceptar conexiones desde localhost y tu dominio
-// Middleware
 const corsOptions = {
-  origin: '*', // Permitir solicitudes desde cualquier origen
-  optionsSuccessStatus: 200 // Algunas versiones de navegadores antiguos (IE11, varios SmartTVs) no manejan bien el status 204
+  origin: ['http://localhost:3000', 'https://www.grupofuturo.com.ar'], // Permitir solo estos orígenes
+  credentials: true, // Permitir credenciales (cookies, autenticación)
+  optionsSuccessStatus: 200
 };
 
-app.use(cors(corsOptions)); // Usar el middleware cors con opciones
-
-app.use(bodyParser.json());
-
+app.use(cors(corsOptions));
 // Servir archivos estáticos desde el directorio 'uploads'
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
