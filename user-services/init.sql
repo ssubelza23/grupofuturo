aca9c5a8666df277e351dabdb51ba76c23d9440f
ssubelza23 <ssubelza23@gmail.com>
2025-02-22 18:15:46 -0300

    section-prjects

--- user-services/init.sql
+++ user-services/init.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) DEFAULT NULL,
-    email VARCHAR(100) DEFAULT NULL,
+    last_name VARCHAR(50),
+    email VARCHAR(100),
     password VARCHAR(100),
-    phone VARCHAR(20) DEFAULT NULL,
-    photo TEXT DEFAULT NULL,
-    user_type VARCHAR(20) CHECK (user_type IN ('comprador', 'vendedor')) DEFAULT NULL,
-    dni VARCHAR(20) DEFAULT NULL,
+    phone VARCHAR(20),
+    photo TEXT,
+    user_type VARCHAR(20) CHECK (user_type IN ('comprador', 'vendedor')),
+    dni VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
