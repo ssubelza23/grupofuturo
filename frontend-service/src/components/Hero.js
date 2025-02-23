d9fc6faf7bc887683cb68dc5533a80579b5da932
ssubelza23 <ssubelza23@gmail.com>
2025-02-23 01:48:49 -0300

    section-prjects

--- frontend-service/src/components/Hero.js
+++ frontend-service/src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
                     <p>Tu inversión segura, tu futuro garantizado. Consulta disponibilidad ahora.</p>
                     <div className="hero-buttons">
                         <a href="/projects" className="btn btn-primary">Ver Proyectos</a>
-                        <a href="#contacto" className="btn btn-secondary">Contáctanos</a>
+                        <a href="https://wa.me/+5492215693951?text=Hola,%20estoy%20interesado%20en%20más%20información%20sobre%20los%20lotes." className="btn btn-secondary" target="_blank" rel="noopener noreferrer">Contáctanos</a>+
                     </div>
                 </div>
             </div>
